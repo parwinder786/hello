0ebcaeccb9878b9d279ce2d1ed45dd0de2ac18bc
arman <parwinderdhaliwal1990@gmail.com>
2021-12-24 12:45:26 -0500

    First Commit

---
+++ Java_programing/src/FirstSteps/Method.java
@@ -0,0 +1,14 @@
+package FirstSteps;
+
+public class Method {
+	public static void main(String[] args) {
+				
+		System.out.println("hello");
+		int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+        
+        System.out.println("max value = "+maxValue);
+        System.out.println("max value = "+minValue);
+	}
+	
+}
